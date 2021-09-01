c6d1c49d9dcf47dc5fef4076f4a8d69df1308b54
rafe <raphael.lafrance@gmail.com>
2021-09-01 12:01:56 -0400

    Still bootstrapping the repository

--- dev_env.bash
+++ dev_env.bash
@@ -1,21 +1,45 @@
 #!/usr/bin/env bash
 
+# #################################################################################
+# Setup the virtual environment for development.
+# You may need to "pip install --user virtualenv" globally.
+# This is not required but some form of project isolation (conda virtual env etc.)
+# is strongly encouraged.
 
 if [[ ! -z "$VIRTUAL_ENV" ]]; then
   echo "'deactivate' before running this script."
   exit 1
 fi
 
-rm -rf venv
-virtualenv -p python3.9 venv
-
-source ./venv/bin/activate
+rm -rf .venv
+virtualenv -p python3.9 .venv
+source ./.venv/bin/activate
 
 python -m pip install --upgrade pip setuptools wheel
 if [ -f requirements.txt ]; then pip install -r requirements.txt; fi
-if [ -f requirements_dev.txt ]; then pip install -r requirements_dev.txt; fi
 
-# pip install -U spacy-nightly --pre
-# python -m spacy download en_core_web_sm
 
-# pip install -e ../traiter
+# ##############################################################################
+# Dev only pip installs (not required b/c they're personal preference)
+
+pip install -U pynvim
+pip install -U 'python-lsp-server[all]'
+pip install -U autopep8 isort pylint yapf pydocstyle
+pip install -U jupyter_nbextensions_configurator jupyterlab_code_formatter
+pip install -U ipyparallel
+
+
+# ###############################################################################
+# Setup pytorch (Uncomment the one that works for your computer. GPU is better.)
+# You will absolutely need one of these.
+
+# pip3 install torch==1.9.0+cu111 torchvision==0.10.0+cu111 torchaudio==0.9.0 -f https://download.pytorch.org/whl/torch_stable.html
+# pip3 install torch==1.9.0+cpu torchvision==0.10.0+cpu torchaudio==0.9.0 -f https://download.pytorch.org/whl/torch_stable.html
+
+
+# ##############################################################################
+# Setup project structure not handled by git. You will want these directories.
+
+mkdir -p data
+mkdir -p models
+mkdir -p output
---
+++ original/phenology/util.py
@@ -0,0 +1,24 @@
+"""Misc. utilities that don't fit in any other module."""
+
+from contextlib import contextmanager
+from tempfile import mkdtemp
+
+
+class DotDict(dict):
+    """Allow dot.notation access to dictionary items."""
+
+    __getattr__ = dict.get
+    __setattr__ = dict.__setitem__
+    __delattr__ = dict.__delitem__
+
+
+
+@contextmanager
+def make_temp_dir(where=None, prefix=None, keep=False):
+    """Handle creation and deletion of temporary directory."""
+    temp_dir = mkdtemp(prefix=prefix, dir=where)
+    try:
+        yield temp_dir
+    finally:
+        if not keep or not where:
+            rmtree(temp_dir, ignore_errors=True)
