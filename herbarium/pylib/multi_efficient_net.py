0e4fe7ac3103de8f74d8ca8a3b4125d6d8eddc7f
rafelafrance <raphael.lafrance@gmail.com>
2021-12-18 07:41:01 -0500

    Add efficient net B7 modelwq

--- herbarium/pylib/multi_efficient_net.py
+++ herbarium/pylib/multi_efficient_net.py
@@ -12,12 +12,10 @@ from .herbarium_dataset import HerbariumDataset
 class MultiEfficientNet(nn.Module):
     """Override EfficientNet so that it uses multiple inputs on the forward pass."""
 
-    def __init__(self, efficient_net, in_feat, orders_len, load_weights, freeze):
+    def __init__(self, efficient_net, orders_len, load_weights, freeze):
         super().__init__()
 
-        dropout = 0.4
-
-        mid_feat = [in_feat // (2 ** i) for i in range(1, 4)]
+        mid_feat = [self.in_feat // (2 ** i) for i in range(1, 4)]
         mix_feat = mid_feat[0] + orders_len
         out_feat = len(HerbariumDataset.all_classes)
 
@@ -29,24 +27,24 @@ class MultiEfficientNet(nn.Module):
                 param.requires_grad = False
 
         self.efficient_net.classifier = nn.Sequential(
-            nn.Dropout(p=dropout, inplace=True),
-            nn.Linear(in_features=in_feat, out_features=mid_feat[0]),
+            nn.Dropout(p=self.dropout, inplace=True),
+            nn.Linear(in_features=self.in_feat, out_features=mid_feat[0]),
             nn.BatchNorm1d(num_features=mid_feat[0]),
             nn.SiLU(inplace=True),
         )
 
         self.multi_classifier = nn.Sequential(
-            nn.Dropout(p=dropout, inplace=True),
+            nn.Dropout(p=self.dropout, inplace=True),
             nn.Linear(in_features=mix_feat, out_features=mid_feat[1]),
             nn.BatchNorm1d(num_features=mid_feat[1]),
             nn.SiLU(inplace=True),
             #
-            nn.Dropout(p=dropout, inplace=True),
+            nn.Dropout(p=self.dropout, inplace=True),
             nn.Linear(in_features=mid_feat[1], out_features=mid_feat[2]),
             nn.BatchNorm1d(num_features=mid_feat[2]),
             nn.SiLU(inplace=True),
             #
-            nn.Dropout(p=dropout, inplace=True),
+            nn.Dropout(p=self.dropout, inplace=True),
             nn.Linear(in_features=mid_feat[2], out_features=out_feat),
             # nn.Softmax(dim=1),
         )
@@ -71,10 +69,11 @@ class MultiEfficientNetB0(MultiEfficientNet):
         self.mean = [0.7743, 0.7529, 0.7100]
         self.std_dev = [0.2250, 0.2326, 0.2449]
 
-        in_feat = 1280
+        self.dropout = 0.2
+        self.in_feat = 1280
         efficient_net = torchvision.models.efficientnet_b0(pretrained=True)
 
-        super().__init__(efficient_net, in_feat, orders_len, load_weights, freeze)
+        super().__init__(efficient_net, orders_len, load_weights, freeze)
 
 
 class MultiEfficientNetB3(MultiEfficientNet):
@@ -85,10 +84,11 @@ class MultiEfficientNetB3(MultiEfficientNet):
         self.mean = [0.7743, 0.7529, 0.7100]
         self.std_dev = [0.2286, 0.2365, 0.2492]  # TODO
 
-        in_feat = 1536
+        self.dropout = 0.3
+        self.in_feat = 1536
         efficient_net = torchvision.models.efficientnet_b3(pretrained=True)
 
-        super().__init__(efficient_net, in_feat, orders_len, load_weights, freeze)
+        super().__init__(efficient_net, orders_len, load_weights, freeze)
 
 
 class MultiEfficientNetB4(MultiEfficientNet):
@@ -99,14 +99,31 @@ class MultiEfficientNetB4(MultiEfficientNet):
         self.mean = [0.7743, 0.7529, 0.7100]
         self.std_dev = [0.2286, 0.2365, 0.2492]
 
-        in_feat = 1792
+        self.dropout = 0.4
+        self.in_feat = 1792
         efficient_net = torchvision.models.efficientnet_b4(pretrained=True)
 
-        super().__init__(efficient_net, in_feat, orders_len, load_weights, freeze)
+        super().__init__(efficient_net, orders_len, load_weights, freeze)
+
+
+class MultiEfficientNetB7(MultiEfficientNet):
+    """A class for training efficient net models."""
+
+    def __init__(self, orders_len, load_weights, freeze):
+        self.size = (600, 600)
+        self.mean = [0.7743, 0.7529, 0.7100]
+        self.std_dev = [0.2286, 0.2365, 0.2492]  # TODO
+
+        self.dropout = 0.5
+        self.in_feat = 2560
+        efficient_net = torchvision.models.efficientnet_b7(pretrained=True)
+
+        super().__init__(efficient_net, orders_len, load_weights, freeze)
 
 
 NETS = {
     "b0": MultiEfficientNetB0,
     "b3": MultiEfficientNetB3,
     "b4": MultiEfficientNetB4,
+    "b7": MultiEfficientNetB7,
 }
